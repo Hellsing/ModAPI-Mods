c8f714b967bfc6be8d646320accf4c1a95a7a242
Hellsing <matrix11@gmx.net>
2016-09-03 12:23:17 +0200

    Added BlockFocusAttack

---
+++ BlockFocusAttack/BlockFocusAttack.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using ModAPI.Attributes;
+using TheForest.Items.Inventory;
+using TheForest.Utils;
+using UnityEngine;
+
+namespace BlockFocusAttack
+{
+    public class BlockFocusAttack : MonoBehaviour
+    {
+        public const string ModName = "BlockFocusAttack";
+
+        public static int LastFocus { get; set; }
+        public static bool ShouldAttack
+        {
+            get { return Environment.TickCount - LastFocus > 200; }
+            set
+            {
+                if (value)
+                {
+                    LastFocus = 0;
+                }
+            }
+        }
+        
+        [ExecuteOnGameStart]
+        // ReSharper disable once UnusedMember.Local
+        private static void Init()
+        {
+            var go = new GameObject($"__{ModName}__");
+            go.AddComponent<BlockFocusAttack>();
+        }
+
+        // ReSharper disable once UnusedMember.Local
+        private void OnApplicationFocus(bool focusStatus)
+        {
+            if (focusStatus)
+            {
+                LastFocus = Environment.TickCount;
+            }
+        }
+    }
+
+    public class PlayerInventoryEx : PlayerInventory
+    {
+        public override void Attack()
+        {
+            if (this == LocalPlayer.Inventory)
+            {
+                if (!BlockFocusAttack.ShouldAttack)
+                {
+                    BlockFocusAttack.ShouldAttack = true;
+                    return;
+                }
+            }
+            base.Attack();
+        }
+    }
+}
