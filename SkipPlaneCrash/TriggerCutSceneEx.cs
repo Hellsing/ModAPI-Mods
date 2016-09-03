c86a7d9cab92a59cfc6a1d00aeee15307f2473f1
Hellsing <matrix11@gmx.net>
2016-09-03 12:23:45 +0200

    Added SkipPlaneCrash

---
+++ SkipPlaneCrash/TriggerCutSceneEx.cs
@@ -0,0 +1,23 @@
+﻿using ModAPI;
+using Input = TheForest.Utils.Input;
+
+namespace SkipPlaneCrash
+{
+    public class TriggerCutSceneEx : TriggerCutScene
+    {
+        protected override void Update()
+        {
+            if (!skipOpening && Clock.planecrash)
+            {
+                SpaceTut.SetActive(false);
+                LightsFlight.SetActive(false);
+                pmTrigger.SendEvent("toSkipOpening");
+                skipOpening = true;
+            }
+            else
+            {
+                Input.LockMouse();
+            }
+        }
+    }
+}
